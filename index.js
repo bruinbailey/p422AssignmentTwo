cff5c11941bd507bd26d0bdd018e58f942fd5ba6
Greg Bailey <bruinbailey@gmail.com>
2018-09-18 15:45:59 -0400

    more experiments with router

--- index.js
+++ index.js
@@ -8,17 +8,6 @@ app.use(bodyParser.json({type: 'application/json'}));
 
 app.use('/v1/password/passwordHealth', router);
 
-app.post('/', function(req, res){
-    if (req.body.password) {
-        console.log(req.body.password);
-        console.table(req.body);
-        res.status(204);
-    } else {
-        res.status(400);
-    }
-    res.end();
-});
-
 
 app.listen(port, function() {
     console.log("Listening on "+ port)
