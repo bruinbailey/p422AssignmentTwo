aa327eaa9576bff467d3974e5f2d53772bd0b1dd
bruinbailey <nnyskrying@hotmail.com>
2018-09-20 13:16:17 -0400

    trying to add the static path for file serving

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(bodyParser.json({type: 'application/json'}));
 
 app.use('/v1', router);
 
+app.use('/static', express.static('public'));
+
 
 app.listen(port, function() {
     console.log("Listening on "+ port)
