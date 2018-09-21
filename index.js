8034f8d8bd49b2ec361f03cfb5d5a0fefe5fb67d
bruinbailey <nnyskrying@hotmail.com>
2018-09-20 21:56:41 -0400

    added comments to index, Bloom, and pwchecker files

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var express = require('express'),
     router = require('./lib/router.js'),
     bodyParser = require('body-parser'),
     port = 8080;
+
 //tells express to use bodyParser to handle requests
 app.use(bodyParser.json({type: 'application/json'}));
+
 // tells express to send all requests starting with /v1 to the router file
 app.use('/v1', router);
+
 // tells express where the static folder for files is.
 app.use('/static', express.static('public'));
 
