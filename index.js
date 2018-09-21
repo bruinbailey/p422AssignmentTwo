d6cdbfe1520a23232504cf6b893d513043862b0f
bruinbailey <nnyskrying@hotmail.com>
2018-09-20 21:35:33 -0400

    starting to add comments for personal reference

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+//imports and sets all needed modules
 var express = require('express'),
     app = express(),
     router = require('./lib/router.js'),
     bodyParser = require('body-parser'),
     port = 8080;
-
+//tells express to use bodyParser to handle requests
 app.use(bodyParser.json({type: 'application/json'}));
-
+// tells express to send all requests starting with /v1 to the router file
 app.use('/v1', router);
-
+// tells express where the static folder for files is.
 app.use('/static', express.static('public'));
 
-
+//opens server to requests on specified port
 app.listen(port, function() {
     console.log("Listening on "+ port)
 })
